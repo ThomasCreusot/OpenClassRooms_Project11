c99c4fac4001057935fea0f38a48683d39f0018c
Thomas Creusot <th.creusot@gmail.com>
2022-10-17 14:52:56 +0200

    test book with correct foudClub and foundCompetition data - should return 200 status code and data

--- tests/unit_tests/test_server.py
+++ tests/unit_tests/test_server.py
@@ -56,3 +56,36 @@ def test_showSummary_valid_unknown_email_should_status_code_500(client):
     assert data.find('The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application.') != -1
 
 
+def test_book_should_status_code_200_and_return_data(client):
+
+    #mettre foundClub = qq chose, idem pour found competition ; vérifier la sortie. 
+    #faire un autre test avec autres valeurs
+    #renommer tests pour explicite.
+    foundClub = {
+        "name":"Simply Lift",
+        "email":"john@simplylift.co",
+        "points":"13"
+    }
+
+    foundCompetition = {
+            "name": "Fall Classic",
+            "date": "2020-10-22 13:30:00",
+            "numberOfPlaces": "13"
+        }
+
+    response = client.get('/book/{0}/{1}'.format(foundCompetition['name'], foundClub["name"]))
+    data = response.data.decode()
+    assert response.status_code == 200
+    assert data.find('<input type="hidden" name="club" value="{0}">'.format(foundClub['name'])) != -1
+    assert data.find('<input type="hidden" name="competition" value="{0}">'.format(foundCompetition['name'])) != -1
+
+
+
+
+
+
+#    if foundClub and foundCompetition:
+#        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+#    else:
+#        flash("Something went wrong-please try again")
+#        return render_template('welcome.html', club=club, competitions=competitions)
