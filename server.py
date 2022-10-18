a5fa37c367717b9a40e86bcaf5c40302c984c0fe
Thomas Creusot <th.creusot@gmail.com>
2022-10-18 15:07:40 +0200

    competition_date_validity() and update welcome.html : no link for booking related to past competition

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import json
+
+from datetime import datetime
+
 from flask import Flask,render_template,request,redirect,flash,url_for
 
 
@@ -20,6 +23,17 @@ app.secret_key = 'something_special'
 competitions = loadCompetitions()
 clubs = loadClubs()
 
+def competition_date_validity(competitions):
+    """Determines for each competition if it is valid or not, based on its date"""
+    for competition in competitions:
+        now = datetime.now(tz=None)
+        competition_time = datetime.strptime(competition["date"], "%Y-%m-%d %H:%M:%S")
+        if competition_time > now:
+            competition["date_validity"]=True
+        elif competition_time <= now:
+            competition["date_validity"]=False
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -27,6 +41,7 @@ def index():
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
     club = [club for club in clubs if club['email'] == request.form['email']][0]
+    competition_date_validity(competitions)
     return render_template('welcome.html',club=club,competitions=competitions, clubs=clubs)
 
 
@@ -38,6 +53,7 @@ def book(competition,club):
         return render_template('booking.html',club=foundClub,competition=foundCompetition)
     else:
         flash("Something went wrong-please try again")
+        competition_date_validity(competitions)
         return render_template('welcome.html', club=club, competitions=competitions)
 
 
@@ -49,6 +65,9 @@ def purchasePlaces():
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     club['points'] = int(club['points'])-placesRequired
     flash('Great-booking complete!')
+
+    competition_date_validity(competitions)
+
     return render_template('welcome.html', club=club, competitions=competitions)
 
 
