0700cff00760af631b454f8e7a7665a69d579c4b
Thomas Creusot <th.creusot@gmail.com>
2022-10-20 16:43:22 +0200

    creation of fixtures for clubs and competitions monkeypathing

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,9 +1,65 @@
 import pytest
 
-from server import app
+import server 
+from server import app, MAX_CLUB_PLACES_PER_COMPETITION
+
 
 @pytest.fixture
 def client():  
     # create_app({"TESTING": True})
     with app.test_client() as client:
         yield client
+
+
+@pytest.fixture
+def basic_competitions_fixture(monkeypatch):
+    test_competitions = [
+        {
+            "name": "Past competition",
+            "date": "2000-01-01 00:00:00",
+            "numberOfPlaces": "1"
+        },
+        {
+            "name": "Future competition",
+            "date": "3000-12-31 00:00:00",
+            "numberOfPlaces": "1"
+        }
+    ]
+    monkeypatch.setattr(server, 'competitions', test_competitions)
+
+
+@pytest.fixture
+def basic_clubs_fixture(monkeypatch):
+    test_clubs = [
+        {
+            "name":"A known club",
+            "email":"test_club@mail.co",
+            "points":"1"
+        }
+    ]
+    monkeypatch.setattr(server, 'clubs', test_clubs)
+
+
+@pytest.fixture
+def clubs_highNumberPoints_fixture(monkeypatch):
+    test_clubs = [
+        {
+            "name":"A club with more than MAX_CLUB_PLACES_PER_COMPETITION",
+            "email": "admin@mail.com",
+            "points":"1"
+        }
+    ]
+    test_clubs[0]['points'] = str(MAX_CLUB_PLACES_PER_COMPETITION + 2)
+    monkeypatch.setattr(server, 'clubs', test_clubs)
+
+
+@pytest.fixture
+def clubs_lowNumberPoints_fixture(monkeypatch):
+    test_clubs = [
+        {
+            "name":"A club with less than MAX_CLUB_PLACES_PER_COMPETITION",
+            "email":"test_club@mail.co",
+            "points":"1"
+        }
+    ]
+    monkeypatch.setattr(server, 'clubs', test_clubs)
--- tests/unit_tests/test_server.py
+++ tests/unit_tests/test_server.py
@@ -4,7 +4,7 @@ from .conftest import client  # fixture import
 
 from server import index, showSummary, book, purchasePlaces, logout
 from server import MAX_CLUB_PLACES_PER_COMPETITION
-
+from .conftest import basic_competitions_fixture, basic_clubs_fixture
 
 def test_index_should_status_code_be_200_and_return_data(client):
     """Tests if index() returns a status code = 200 and expected data"""
@@ -18,33 +18,10 @@ def test_index_should_status_code_be_200_and_return_data(client):
     assert data.find("Please enter your secretary email to continue:") != -1
 
 
-def test_showSummary_valid_known_email_should_status_code_200_and_return_data(client, monkeypatch):
+def test_showSummary_valid_known_email_should_status_code_200_and_return_data(client, basic_competitions_fixture, basic_clubs_fixture):
     """Tests if showSummary() returns a status code = 200 and expected data when email is known
     from the json database. Expected data differs between past competitions and futures
     competitions"""
-
-    test_competitions = [
-        {
-            "name": "Past competition",
-            "date": "2000-01-01 00:00:00",
-            "numberOfPlaces": "1"
-        },
-        {
-            "name": "Future competition",
-            "date": "3000-12-31 00:00:00",
-            "numberOfPlaces": "1"
-        }
-    ]
-    monkeypatch.setattr(server, 'competitions', test_competitions)
-
-    test_clubs = [
-        {
-            "name":"A known club",
-            "email":"test_club@mail.co",
-            "points":"1"
-        }
-    ]
-    monkeypatch.setattr(server, 'clubs', test_clubs)
 
     club = {
         "name":"A known club",
@@ -78,18 +55,9 @@ def test_showSummary_valid_known_email_should_status_code_200_and_return_data(cl
                      '                <a href="/book/Future%20competition/A%20known%20club">Book Places</a>') != -1
 
 
-def test_showSummary_valid_unknown_email_should_status_code_302(client, monkeypatch):
+def test_showSummary_valid_unknown_email_should_status_code_302(client, basic_clubs_fixture):
     """Tests if showSummary() returns a status code = 200 and expected data when email is not known
     from the json database."""
-
-    test_clubs = [
-        {
-            "name":"A known club",
-            "email":"test_club@mail.co",
-            "points":"1"
-        }
-    ]
-    monkeypatch.setattr(server, 'clubs', test_clubs)
 
     club = {
         "name":"An unknown club",
@@ -106,33 +74,9 @@ def test_showSummary_valid_unknown_email_should_status_code_302(client, monkeypa
     assert data.find('Unknown email adress') != -1
 
 
-def test_book_should_status_code_200_and_return_data(client, monkeypatch):
+def test_book_should_status_code_200_and_return_data(client, basic_competitions_fixture, basic_clubs_fixture):
     """Tests if book() returns a status code = 200 and expected data when club and competition are
     known from the json database."""
-
-    test_competitions = [
-        {
-            "name": "Past competition",
-            "date": "2000-01-01 00:00:00",
-            "numberOfPlaces": "1"
-        },
-        {
-            "name": "Future competition",
-            "date": "3000-12-31 00:00:00",
-            "numberOfPlaces": "1"
-        }
-    ]
-    monkeypatch.setattr(server, 'competitions', test_competitions)
-
-    test_clubs = [
-        {
-            "name":"A known club",
-            "email":"test_club@mail.co",
-            "points":"1"
-        }
-    ]
-    monkeypatch.setattr(server, 'clubs', test_clubs)
-
 
     foundClub = {
         "name":"A known club",
@@ -153,33 +97,9 @@ def test_book_should_status_code_200_and_return_data(client, monkeypatch):
     assert data.find('<input type="hidden" name="competition" value="{0}">'.format(foundCompetition['name'])) != -1
 
 
-def test_book_invalid_club_should_status_code_200_and_return_data(client, monkeypatch):
+def test_book_invalid_club_should_status_code_200_and_return_data(client, basic_competitions_fixture, basic_clubs_fixture):
     """Tests if book() returns a status code = 200 and expected data when competition is known from
     the json database but club is not."""
-
-    test_competitions = [
-        {
-            "name": "Past competition",
-            "date": "2000-01-01 00:00:00",
-            "numberOfPlaces": "1"
-        },
-        {
-            "name": "Future competition",
-            "date": "3000-12-31 00:00:00",
-            "numberOfPlaces": "1"
-        }
-    ]
-    monkeypatch.setattr(server, 'competitions', test_competitions)
-
-    test_clubs = [
-        {
-            "name":"A known club",
-            "email":"test_club@mail.co",
-            "points":"1"
-        }
-    ]
-    monkeypatch.setattr(server, 'clubs', test_clubs)
-
 
     foundClub = {
         "name":"An unknown club",
@@ -196,33 +116,9 @@ def test_book_invalid_club_should_status_code_200_and_return_data(client, monkey
     assert response.status_code == 500
 
 
-def test_book_invalid_competition_should_status_code_200_and_return_data(client, monkeypatch):
+def test_book_invalid_competition_should_status_code_200_and_return_data(client, basic_competitions_fixture, basic_clubs_fixture):
     """Tests if book() returns a status code = 200 and expected data when club is known from
     the json database but competition is not."""
-
-    test_competitions = [
-        {
-            "name": "Past competition",
-            "date": "2000-01-01 00:00:00",
-            "numberOfPlaces": "1"
-        },
-        {
-            "name": "Future competition",
-            "date": "3000-12-31 00:00:00",
-            "numberOfPlaces": "1"
-        }
-    ]
-    monkeypatch.setattr(server, 'competitions', test_competitions)
-
-    test_clubs = [
-        {
-            "name":"A known club",
-            "email":"test_club@mail.co",
-            "points":"1"
-        }
-    ]
-    monkeypatch.setattr(server, 'clubs', test_clubs)
-
 
     foundClub = {
             "name":"A known club",
@@ -240,7 +136,7 @@ def test_book_invalid_competition_should_status_code_200_and_return_data(client,
     assert response.status_code == 500
 
 
-def test_purchasePlaces_should_status_code_200_update_points_and_return_data(client, monkeypatch):
+def test_purchasePlaces_should_status_code_200_update_points_and_return_data(client, basic_competitions_fixture, basic_clubs_fixture):
     """Tests if purchasePlaces() returns a status code = 200 and expected data when club and
     competition are known from the database, and placesRequired is correct
     
@@ -248,29 +144,6 @@ def test_purchasePlaces_should_status_code_200_update_points_and_return_data(cli
     placesRequired < MAX_CLUB_PLACES_PER_COMPETITION
     placesRequired <= club['points']
     """
-
-    test_competitions = [
-        {
-            "name": "Past competition",
-            "date": "2000-01-01 00:00:00",
-            "numberOfPlaces": "1"
-        },
-        {
-            "name": "Future competition",
-            "date": "3000-12-31 00:00:00",
-            "numberOfPlaces": "1"
-        }
-    ]
-    monkeypatch.setattr(server, 'competitions', test_competitions)
-
-    test_clubs = [
-        {
-            "name":"A known club",
-            "email":"test_club@mail.co",
-            "points":"1"
-        }
-    ]
-    monkeypatch.setattr(server, 'clubs', test_clubs)
 
     competition = {
             "name": "Future competition",
@@ -304,34 +177,10 @@ def test_purchasePlaces_should_status_code_200_update_points_and_return_data(cli
     assert data.find('Number of Places: {0}'.format(expected_competition_places)) != -1
 
 
-def test_purchasePlaces_should_status_code_200_flash_too_much_placesRequired_one_booking(client, monkeypatch):
+def test_purchasePlaces_should_status_code_200_flash_too_much_placesRequired_one_booking(client, basic_competitions_fixture, clubs_highNumberPoints_fixture):
     """Tests if purchasePlaces() returns a status code = 200 and expected data when club and
     competition are known from the database, but placesRequired is higher than 
     MAX_CLUB_PLACES_PER_COMPETITION"""
-
-    test_competitions = [
-        {
-            "name": "Past competition",
-            "date": "2000-01-01 00:00:00",
-            "numberOfPlaces": "1"
-        },
-        {
-            "name": "Future competition",
-            "date": "3000-12-31 00:00:00",
-            "numberOfPlaces": "1"
-        }
-    ]
-
-    test_clubs = [
-        {
-            "name":"A club with more than MAX_CLUB_PLACES_PER_COMPETITION",
-            "email": "admin@mail.com",
-            "points":"14"
-        }]
-
-    monkeypatch.setattr(server, 'competitions', test_competitions)
-    monkeypatch.setattr(server, 'clubs', test_clubs)
-
 
     competition = {
             "name": "Future competition",
@@ -342,7 +191,8 @@ def test_purchasePlaces_should_status_code_200_flash_too_much_placesRequired_one
     club = {
             "name":"A club with more than MAX_CLUB_PLACES_PER_COMPETITION",
             "email": "admin@mail.com",
-            "points":"14"
+            # // conftest.py clubs_highNumberPoints_fixture
+            "points": str(MAX_CLUB_PLACES_PER_COMPETITION + 2)
     }
 
     placesRequired = MAX_CLUB_PLACES_PER_COMPETITION + 1
@@ -365,33 +215,10 @@ def test_purchasePlaces_should_status_code_200_flash_too_much_placesRequired_one
     assert data.find('Number of Places: {0}'.format(expected_competition_places)) != -1
 
 
-def test_purchasePlaces_should_status_code_200_flash_too_much_placesRequired_several_bookings(client, monkeypatch):
+def test_purchasePlaces_should_status_code_200_flash_too_much_placesRequired_several_bookings(client, basic_competitions_fixture, clubs_highNumberPoints_fixture):
     """Tests if purchasePlaces() returns a status code = 200 and expected data when club and
     competition are known from the database, placesRequired of several bookings are corrects but
     the sum of these placesRequired is higher than MAX_CLUB_PLACES_PER_COMPETITION"""
-
-    test_competitions = [
-        {
-            "name": "Past competition",
-            "date": "2000-01-01 00:00:00",
-            "numberOfPlaces": "1"
-        },
-        {
-            "name": "Future competition",
-            "date": "3000-12-31 00:00:00",
-            "numberOfPlaces": "1"
-        }
-    ]
-
-    test_clubs = [
-        {
-            "name":"A club with more than MAX_CLUB_PLACES_PER_COMPETITION",
-            "email": "admin@mail.com",
-            "points":"14"
-        }]
-
-    monkeypatch.setattr(server, 'competitions', test_competitions)
-    monkeypatch.setattr(server, 'clubs', test_clubs)
 
     competition = {
             "name": "Future competition",
@@ -402,8 +229,10 @@ def test_purchasePlaces_should_status_code_200_flash_too_much_placesRequired_sev
     club = {
             "name":"A club with more than MAX_CLUB_PLACES_PER_COMPETITION",
             "email": "admin@mail.com",
-            "points":"14"
+            # // conftest.py clubs_highNumberPoints_fixture
+            "points": str(MAX_CLUB_PLACES_PER_COMPETITION + 2)
     }
+
 
     # First booking
     placesRequired = MAX_CLUB_PLACES_PER_COMPETITION - 1
@@ -434,33 +263,9 @@ def test_purchasePlaces_should_status_code_200_flash_too_much_placesRequired_sev
     ))
 
 
-def test_purchasePlaces_should_status_code_200_flash_too_less_club_points(client, monkeypatch):
+def test_purchasePlaces_should_status_code_200_flash_too_less_club_points(client, basic_competitions_fixture, clubs_lowNumberPoints_fixture):
     """Tests if purchasePlaces() returns a status code = 200 and expected data when club and
     competition are known from the database, but placesRequired is higher than club['points']"""
-
-    test_competitions = [
-        {
-            "name": "Past competition",
-            "date": "2000-01-01 00:00:00",
-            "numberOfPlaces": "1"
-        },
-        {
-            "name": "Future competition",
-            "date": "3000-12-31 00:00:00",
-            "numberOfPlaces": "1"
-        }
-    ]
-    monkeypatch.setattr(server, 'competitions', test_competitions)
-
-    test_clubs = [
-        {
-            "name":"A club with less than MAX_CLUB_PLACES_PER_COMPETITION",
-            "email":"test_club@mail.co",
-            "points":"1"
-        }
-    ]
-    monkeypatch.setattr(server, 'clubs', test_clubs)
-
 
     competition = {
             "name": "Future competition",
@@ -474,7 +279,6 @@ def test_purchasePlaces_should_status_code_200_flash_too_less_club_points(client
             "points":"1"
         }
 
-
     placesRequired = int(club["points"]) + 1
 
     return_value = client.post("/purchasePlaces",
@@ -482,7 +286,6 @@ def test_purchasePlaces_should_status_code_200_flash_too_less_club_points(client
                            'club': club["name"],
                            'places': placesRequired}
     )
-
 
     assert return_value.status_code == 200
     data = return_value.data.decode()
